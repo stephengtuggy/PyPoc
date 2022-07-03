daa01d2f413b5c876bdbb17313087f8105c65016
Stephen G Tuggy <sgt@stephengtuggy.com>
2022-07-02 17:40:04 -0700

    main.cpp: Add PY_SSIZE_T_CLEAN define and boost::python::import(...); minimal reproducible crashing example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <boost/python.hpp>
 #include <iostream>
 
 int main() {
     Py_Initialize();
+
+    boost::python::import("__main__");
 
     std::cout << "Hello, World!" << std::endl;
 
