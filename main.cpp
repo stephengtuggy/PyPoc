95164a7a2c9f4844e865f691786882854bb7c55c
Stephen G Tuggy <sgt@stephengtuggy.com>
2022-07-03 07:53:06 -0700

    main.cpp: Experimenting with following the documentation ;-)

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
+//#define PY_SSIZE_T_CLEAN
+//#include <Python.h>
 #include <boost/python.hpp>
-#include <iostream>
+//#include <iostream>
+//#include <stdlib.h>
 
 int main() {
+//    setenv("PYTHONPATH", ".", 1);
     Py_Initialize();
 
-    boost::python::import("__main__");
+    try {
+        boost::python::object main_module = boost::python::import("__main__");
+        boost::python::object main_namespace = main_module.attr("__dict__");
+        boost::python::object ignored = boost::python::exec("print(\"Hello world from Python!\"", main_namespace);
+    } catch (const boost::python::error_already_set&) {
+        PyErr_Print();
+        return 1;
+    }
 
-    std::cout << "Hello, World!" << std::endl;
-
-    Py_Finalize();
     return 0;
 }
+
+//PyObject* MyPyFunc() {
+//
+//}
