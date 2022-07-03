dcf69454517adac7babb08355ae7a2a9033e864b
Stephen G Tuggy <sgt@stephengtuggy.com>
2022-07-02 17:33:09 -0700

    More on CMakeLists.txt; begin including Python-specific code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#include <Python.h>
+#include <boost/python.hpp>
 #include <iostream>
 
 int main() {
+    Py_Initialize();
+
     std::cout << "Hello, World!" << std::endl;
+
+    Py_Finalize();
     return 0;
 }
