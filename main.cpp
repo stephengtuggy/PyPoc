2c3193735ea7080c26024d1a513d7cee1268eca8
Stephen G Tuggy <sgt@stephengtuggy.com>
2025-08-11 21:35:57 -0700

    WIP on opentelemetry-cpp stuff
    Signed-off-by: Stephen G Tuggy <sgt@stephengtuggy.com>

--- main.cpp
+++ main.cpp
@@ -4,6 +4,34 @@
 //#include <iostream>
 //#include <stdlib.h>
 
+#if defined(USE_OPEN_TELEMETRY)
+#include <functional>
+#include <iostream>
+#include <string>
+#include <utility>
+
+// #include "opentelemetry/exporters/otlp/otlp_file_client_options.h"
+// #include "opentelemetry/exporters/otlp/otlp_file_exporter_factory.h"
+// #include "opentelemetry/exporters/otlp/otlp_file_exporter_options.h"
+// #include "opentelemetry/exporters/otlp/otlp_file_log_record_exporter_factory.h"
+// #include "opentelemetry/exporters/otlp/otlp_file_log_record_exporter_options.h"
+#include "opentelemetry/logs/logger_provider.h"
+#include "opentelemetry/nostd/shared_ptr.h"
+#include "opentelemetry/sdk/logs/exporter.h"
+#include "opentelemetry/sdk/logs/logger_provider.h"
+#include "opentelemetry/sdk/logs/logger_provider_factory.h"
+#include "opentelemetry/sdk/logs/processor.h"
+#include "opentelemetry/sdk/logs/provider.h"
+#include "opentelemetry/sdk/logs/simple_log_record_processor_factory.h"
+#include "opentelemetry/sdk/trace/exporter.h"
+#include "opentelemetry/sdk/trace/processor.h"
+#include "opentelemetry/sdk/trace/provider.h"
+#include "opentelemetry/sdk/trace/simple_processor_factory.h"
+#include "opentelemetry/sdk/trace/tracer_provider.h"
+#include "opentelemetry/sdk/trace/tracer_provider_factory.h"
+#include "opentelemetry/trace/tracer_provider.h"
+#endif
+
 int main() {
 //    setenv("PYTHONPATH", ".", 1);
 //    std::string modpaths("\"\",");
