e4be0f3cd8b842402bb6a32908232e60b4875abd
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-12-22 20:34:14 -0800

    Update 3x .github/workfows/*-CI.yml; omit x64-linux-sgt.cmake; update CMakeLists.txt; update vcpkg.json and vcpkg-configuration.json

--- script/local-test-ci.sh
+++ script/local-test-ci.sh
@@ -1,1 +1,1 @@
-script/cibuild --preset-name="linux-ninja-pie-enabled-debug-use-otel" --from="fedora:42"
+script/cibuild --preset-name="linux-ninja-pie-enabled-debug-fetch-use-otel" --from="fedora:42"
